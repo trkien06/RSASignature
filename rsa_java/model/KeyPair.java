fa708b170b2f432c80735b60914d5a5b0bc034cd
trkien06 <vdtkypnd@gmail.com>
2026-06-09 13:46:29 +0700

    Update KeyPair.java

--- rsa_java/model/KeyPair.java
+++ rsa_java/model/KeyPair.java
@@ -2,21 +2,22 @@ package model;
 
 /**
  * Cặp khóa RSA: gộp PublicKey + PrivateKey.
- * AlgorithmRSA.generateKey() trả về object này.
  */
 public class KeyPair {
 
-    public final PublicKey  publicKey;
-    public final PrivateKey privateKey;
+    private final PublicKey publicKey;
+    private final PrivateKey privateKey;
 
     public KeyPair(PublicKey publicKey, PrivateKey privateKey) {
-        this.publicKey  = publicKey;
+        this.publicKey = publicKey;
         this.privateKey = privateKey;
     }
+
+    public PublicKey getPublicKey() { return publicKey; }
+    public PrivateKey getPrivateKey() { return privateKey; }
 
     @Override
     public String toString() {
         return "KeyPair{\n  " + publicKey + "\n  " + privateKey + "\n}";
     }
 }
-
