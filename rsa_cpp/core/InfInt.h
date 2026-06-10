d0a4b8f45eded5481ac7a5fdee2ad6ae003e2fae
trkien06 <vdtkypnd@gmail.com>
2026-06-10 12:56:15 +0700

    Update InfInt.h

--- rsa_cpp/core/InfInt.h
+++ rsa_cpp/core/InfInt.h
@@ -1,1 +1,6 @@
+#pragma once
+#include <boost/multiprecision/cpp_int.hpp>
+
+// Biến tên gọi InfInt cũ thành kiểu dữ liệu cpp_int siêu tốc của Boost
+using InfInt = boost::multiprecision::cpp_int;
 
