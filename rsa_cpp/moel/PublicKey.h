a2edfeae5815da17cd5e4a4e52d17f07deb8faa6
trkien06 <vdtkypnd@gmail.com>
2026-06-10 12:52:34 +0700

    Update PublicKey.h

--- rsa_cpp/moel/PublicKey.h
+++ rsa_cpp/moel/PublicKey.h
@@ -1,1 +1,13 @@
+#pragma once
+#include "../core/InfInt.h"
+
+namespace model {
+    class PublicKey {
+    public:
+        InfInt n;
+        InfInt e;
+
+        PublicKey(InfInt n_val = 0, InfInt e_val = 0) : n(n_val), e(e_val) {}
+    };
+}
 
