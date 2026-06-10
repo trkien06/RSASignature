3a2a2f9a9a240afbc331003404d7555d770b1ca2
trkien06 <vdtkypnd@gmail.com>
2026-06-10 12:55:48 +0700

    Update AlgorithmRSA.h

--- rsa_cpp/core/AlgorithmRSA.h
+++ rsa_cpp/core/AlgorithmRSA.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
+#include "InfInt.h"
+
+namespace core {
+    class AlgorithmRSA {
+    public:
+        static InfInt gcd(InfInt a, InfInt b);
+        static bool isPrime(InfInt n);
+        static InfInt modInverse(InfInt e, InfInt phi);
+        static InfInt powerMod(InfInt base, InfInt exp, InfInt mod);
+        static std::wstring CalculateSHA256(const std::wstring& filePath);
+        static InfInt HashToInt(const std::wstring& hashHex);
+    };
+}
 
