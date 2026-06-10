9ae844ac829a3c4ba86e83eb6d9af420443798d6
trkien06 <vdtkypnd@gmail.com>
2026-06-10 12:53:17 +0700

    Update PrivateKey.h

--- rsa_cpp/moel/PrivateKey.h
+++ rsa_cpp/moel/PrivateKey.h
@@ -1,1 +1,12 @@
+#pragma once
+#include "../core/InfInt.h"
 
+namespace model {
+    class PrivateKey {
+    public:
+        InfInt n;
+        InfInt d;
+
+        PrivateKey(InfInt n_val = 0, InfInt d_val = 0) : n(n_val), d(d_val) {}
+    };
+}
