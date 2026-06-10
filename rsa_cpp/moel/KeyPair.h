7e90b2fc7647b40201f8ce1d2cb0052066aaa836
trkien06 <vdtkypnd@gmail.com>
2026-06-10 12:53:39 +0700

    Update KeyPair.h

--- rsa_cpp/moel/KeyPair.h
+++ rsa_cpp/moel/KeyPair.h
@@ -1,1 +1,15 @@
+#pragma once
+#include "PublicKey.h"
+#include "PrivateKey.h"
+
+namespace model {
+    class KeyPair {
+    public:
+        PublicKey publicKey;
+        PrivateKey privateKey;
+
+        KeyPair(PublicKey pub, PrivateKey priv) : publicKey(pub), privateKey(priv) {}
+        KeyPair() {}
+    };
+}
 
